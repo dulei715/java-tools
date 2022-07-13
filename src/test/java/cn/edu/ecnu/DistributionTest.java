79a3581168298b8a1bb48df54906a5d49fed6212
dulei715 <dulei715@126.com>
2022-07-14 00:57:43 +0800

    aa

---
+++ src/test/java/cn/edu/ecnu/DistributionTest.java
@@ -0,0 +1,11 @@
+package cn.edu.ecnu;
+
+import org.apache.commons.math3.analysis.function.Gaussian;
+import org.junit.Test;
+
+public class DistributionTest {
+    @Test
+    public void fun1() {
+
+    }
+}
