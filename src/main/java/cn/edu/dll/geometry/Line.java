39177a2f1ddac97a121e8e93d8fe0693264bcaaf
Leilei Du <dulei715@126.com>
2024-10-22 09:57:58 +0800

    Saved by Leilei through a script

--- src/main/java/cn/edu/dll/geometry/Line.java
+++ src/main/java/cn/edu/dll/geometry/Line.java
@@ -14,6 +14,24 @@ public class Line {
         this.paramC = paramC;
     }
 
+    public Line(double x1, double y1, double x2, double y2) {
+        this.paramA = y2 - y1;
+        this.paramB = x1 - x2;
+        this.paramC = x2 * y1 - x1 * y2;
+    }
+
+    /**
+     * 将this.paramA设为正的e而不改变直线
+     */
+    public void unifyXParamTag() {
+        if (this.paramA >= 0) {
+            return;
+        }
+        this.paramA = -this.paramA;
+        this.paramB = -this.paramB;
+        this.paramC = -this.paramC;
+    }
+
     public Line roll(double xIndex, double yIndex, double thetaAngle) {
         double newParamA = this.paramA * Math.cos(thetaAngle) - this.paramB * Math.sin(thetaAngle);
         double newParamB = this.paramA * Math.sin(thetaAngle) + this.paramB * Math.cos(thetaAngle);
@@ -24,6 +42,30 @@ public class Line {
     @Override
     public String toString() {
         return paramA + "x" + (paramB < 0 ? "" : "+") + paramB + "y" + (paramC < 0 ? "" : "+") + paramC + "=0";
+    }
+
+    public double getParamA() {
+        return paramA;
+    }
+
+    public void setParamA(double paramA) {
+        this.paramA = paramA;
+    }
+
+    public double getParamB() {
+        return paramB;
+    }
+
+    public void setParamB(double paramB) {
+        this.paramB = paramB;
+    }
+
+    public double getParamC() {
+        return paramC;
+    }
+
+    public void setParamC(double paramC) {
+        this.paramC = paramC;
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/cn/edu/dll/geometry/LineUtils.java
@@ -0,0 +1,31 @@
+package cn.edu.dll.geometry;
+
+public class LineUtils {
+    /**
+     * 判断给定点是否在给定所有直线的同一侧（点是否在直线围成的图形内）
+     * @param xIndex
+     * @param yIndex
+     * @param lines
+     * @return
+     */
+    public static boolean isInnerGeo(double xIndex, double yIndex, Line... lines) {
+        double tempA, tempB, tempC;
+        int i;
+        boolean judge;
+        lines[0].unifyXParamTag();
+        tempA = lines[0].getParamA();
+        tempB = lines[0].getParamB();
+        tempC = lines[0].getParamC();
+        judge = tempA * xIndex + tempB * yIndex + tempC <= 0;
+        for (i = 1; i < lines.length; ++i) {
+            lines[i].unifyXParamTag();
+            tempA = lines[i].getParamA();
+            tempB = lines[i].getParamB();
+            tempC = lines[i].getParamC();
+            if (judge != (tempA * xIndex + tempB * yIndex + tempC <= 0)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
